7a949250fa96aeb6d336d4b4e092fbe4dfef7148
Quazimodus <diablero797@gmail.com>
2019-10-17 16:35:10 +0300

    minor revision

--- backup-script/back_on_zipfile_V12.py
+++ backup-script/back_on_zipfile_V12.py
@@ -6,21 +6,21 @@ import os
 import time
 import zipfile
 from tkinter import messagebox as mb
-
-# Директория для создания архива
+# ======================================== Пути =====
 # source = input('Укажите файл для упаковки в zip архив -->')
-source = "C:\\TEST_1C\\1C\\"
+
+# source = "C:\\TEST_1C\\1C\\" # Путь для тестирования
+source = "\\\server\\1C\\"
 
 # 2. Резервные копии должны храниться в основном каталоге резерва.
 target_dir = 'E:\\BACKUP\\AUTOBACK_1C\\'  # Подставьте тот путь, который вы будете использовать.
-
+# ====================================================
 # Создаём каталог, если его ещё нет
 if not os.path.exists(target_dir):
     os.mkdir(target_dir)  # создание каталога
     print('Каталог успешно создан', target_dir)
 
 # 3. Файлы помещаются в zip-архив.
-
 # 4. Текущая дата служит именем подкаталога в основном каталоге
 today = target_dir + os.sep + time.strftime("%Y-%m-%d", time.localtime())
 
@@ -34,12 +34,12 @@ if not os.path.exists(today):
     os.mkdir(today)  # создание каталога
     print('Каталог успешно создан', today)
 mode = zipfile.ZIP_DEFLATED
-zipp = zipfile.ZipFile(target, 'w', mode)  # создание архива
+zip_make = zipfile.ZipFile(target, 'w', mode)  # создание архива
 for root, dirs, files in os.walk(source):  # получаем адрес каталога и имена подкатологов и файлов
     for file in files:
-        zipp.write(os.path.join(root, file))  # пишем файлы в архив
+        zip_make.write(os.path.join(root, file))  # пишем файлы в архив
 
-zipp.close()
+zip_make.close()
 # Добавляем вывод информации после окнчания архивирования
 mb.showinfo("Сообщение", f"Бекап готов! \nПуть: {target_dir}")
 
